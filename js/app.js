a00b5c8df2003140d69fa5f69a908a6c9f9f4e42
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-18 15:21:27 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -1,37 +1,56 @@
-const loadData =async (input = '')=>{
+const loadAllData = (input = '')=>{
 
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`;
+    loadData(url,displayData)
+}
+
+const loadData =async (url,callBack) =>{
     try{
 
         const res = await fetch(url);
         const data = await res.json();
-        displayData(data.drinks)
+        callBack(data.drinks)
     }catch(e){
 
     }
 }
 
-// intial call 
-loadData();
 
 const displayData = (drinks) =>{
     const cocktailContainer = document.getElementById('cocktail-container')
 
-    const htmlDocument = drinks.map(drink => getHtmlDocument(drink))
-    console.log(htmlDocument)
+    const htmlDocument = drinks.map(drink => getHtmlDocument(drink));
     cocktailContainer.innerHTML = htmlDocument.join(' ');
 }
 
-const getHtmlDocument = ({strDrinkThumb,strAlcoholic,strInstructions}) =>{
+const getHtmlDocument = ({strDrinkThumb,strAlcoholic,strInstructions,idDrink}) =>{
     return `
     <div class="col">
-                  <div class="card">
+                  <div class="card h-100">
                     <img src="${strDrinkThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">${strAlcoholic}</h5>
                       <p class="card-text">${strInstructions}</p>
                     </div>
+                    <div class="text-center">
+                    <button onclick="loadCocktail('${idDrink}')" class="mb-4  btn btn-primary"data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button></div>
                   </div>
                 </div>
     `;
 }
+
+const loadCocktail = (id) =>{
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    loadData(url,detailCocktail)
+}
+const detailCocktail = (drink)=>{
+    console.log(drink)
+    document.getElementById('exampleModalLabel').innerText = `${drink[0].strIngredient2}`
+    document.getElementById('modal-img').setAttribute('src', drink[0].strDrinkThumb);
+    document.getElementById('category').innerText= drink[0].strCategory;
+    document.getElementById('name').innerText = drink[0].strIngredient2;
+}
+
+
+// intial call 
+loadAllData();
